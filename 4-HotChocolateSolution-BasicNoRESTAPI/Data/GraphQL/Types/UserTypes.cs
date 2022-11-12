7581f6f3f314252e43adfdf03b5961f0b443e013
Allan Levsen <allanlevsen@gmail.com>
2022-11-12 10:03:42 -0700

    Allan development sprint6 (#1)
    * Added HotChocolateDemo (Basic - No API)
    
    * Small changes to the BASICNoRestApi - 4 - version
    
    Co-authored-by: Allan Levsen <LevsenA@macewan.ca>

---
+++ 4-HotChocolateSolution-BasicNoRESTAPI/Data/GraphQL/Types/UserTypes.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace Data.GraphQL.Types
+{
+    // These GraphQL types are used for User mutations
+    //
+    public record UserInput(
+        int UserId,
+        string? FirstName,
+        string? LastName
+    );
+
+    public record UserPayload(
+        User user
+    );
+}
